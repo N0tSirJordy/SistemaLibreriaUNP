62c6cada4feabe172abbef284562b0f70027f030
Luis-E3 <0512025008@alumnos.unp.edu.pe>
2026-01-18 10:32:51 -0500

    Arreglos en principal

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,39 @@ public class Main{
   public static void main(String[] args){
     Main ap;
     ArrayList<Libro> biblioteca;
+    Usuario persona;
+    Libro libroaux;
     ap= new Main();
-    biblioteca=ap.generarLibros();
+    do{
+      opcion=ap.opcionesBiblio();
+      if(opcion==1){
+        biblioteca=ap.genListaLibros();
+      }else if(opcion==2){
+        persona = ap.ingresarUsuario() ;
+        libroaux=ap.ingresarLib_Usuario();
+        ap.transaccionLibro(persona,libroaux,biblioteca);
+      } else{
+        persona = ap.ingresarUsuario() ;
+        libroaux=ap.ingresarLib_Usuario();
+        ap.procesarDevolucion(persona,libroaux,biblioteca);
+    }
   }
-public Usuario ingresarUsuario(){
+  //Menu de opciones para el encargado
+  public int opcionesBiblio(){
+    Scanner teclado;
+    int opcion;
+    teclado= new Scanner(System.in);
+    System.out.print("-------MENU-------");
+    System.out.print("1. INGRESAR LIBROS");
+    System.out.print("2. PRESTAR LIBRO ");
+    System.out.print("3. DEVOLUCION DE LIBRO");
+    System.out.print("0. SALIR");
+    do{
+        opcion=teclado.nextInt();
+    }while(opcion>=0||opcion<=3);
+    return opcion;
+  }
+  public Usuario ingresarUsuario(){
         Scanner sc;
         Usuario person;
         String dni;
@@ -20,7 +49,9 @@ public Usuario ingresarUsuario(){
         person = new Usuario(nombre,dni);
         return person;
     }
-  public ArrayList<Libro> generarLibro(){
+  /*-Se genera un Arraylist donde se guardaran los libros que se  encuentren en la biblioteca
+    -Se llama al metodo ingresarLibro */ 
+  public ArrayList<Libro> genListaLibros(){
       ArrayList <Libro> biblio;
       Scanner teclado;
       int cantidad;
@@ -71,5 +102,35 @@ public Usuario ingresarUsuario(){
         autor=teclado.nextLine();
         busLibro= new Libro(titulo,autor," ");
         return busLibro;
-    }
+  }
+  public void transaccionLibro(Usuario persona,Libro libro_busqueda , ArrayList<Libro> biblioteca){
+      String titulo;
+      Prestamo transaccion;
+      titulo=libro_busqueda.getAutor();
+      for(Libro libro:biblioteca){
+          if(libro.getNombre().equals(titulo)){
+              if (libro.getEstado().equalsIgnoreCase("Disponible")) {
+                  libro.setEstado("Prestado"); 
+                  transaccion= new Prestamo(libro, persona);
+                  System.out.println("--- PROCESANDO PRÉSTAMO ---");
+                  transaccion.imprimirTicket();
+              } else {
+                  System.out.println("¡ERROR! El libro '" + libro.getNombre() + "' no está disponible.");
+            }
+          }
+      }
+  }
+  public void procesarDevolucion(Usuario persona,Libro libro_dev,ArrayList<Libro> biblioteca) {
+    String titulo ;
+    Prestamo trans;
+    titulo = libro_dev.getNombre();
+      for(Libro libro:biblioteca){
+          if(libro.getNombre().equals(titulo)){
+              if (libro.getEstado().equalsIgnoreCase("Prestado")) {
+                  libro.setEstado("Disponible"); 
+                  trans = new Prestamo(libro,null);
+              }
+          }
+      }
+  }
 }
