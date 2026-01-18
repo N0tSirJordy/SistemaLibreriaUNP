2fd937b2cd4278305820e2764ee63519accd784f
NotSirJordy <75282138+N0tSirJordy@users.noreply.github.com>
2026-01-17 22:56:26 -0500

    Create Usuario.java
    Creacion de la clase Usuario con sus respectivos detalles(dni, nombre)
    Getter de dni tambien añadido

---
+++ src/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario{
+    private String nombre_Alum;
+    private String dni;
+    
+    public Usuario(String Nombre_Alum,String dni){
+        this.nombre_Alum=Nombre_Alum;
+        this.dni=dni;
+    }
+    public String getNombre_Alum(){
+        return nombre_Alum;
+    }
+    public String getDNI(){
+        return dni;
+    }
+}
